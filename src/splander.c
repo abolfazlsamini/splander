16a0bc57c0bc0665e7bd997aaa27d03cc2287ea6
mahanfr <mahan.farzaneh@gmail.com>
2024-12-03 12:45:36 +0330

    Resolve merge conflict

--- src/splander.c
+++ src/splander.c
@@ -6,49 +6,57 @@
 #else
 #include "raylib.h"
 #endif
+#include <stdlib.h>
 
-Camera camera = { 0 };
-Vector3 cubePosition = { 0 };
+typedef struct {
+    Vector2 pos;
+    Vector2 size;
+} Player;
+
+typedef struct {
+    Player player;
+} GameState;
 
 // Update and draw one frame
-static void UpdateDrawFrame(void);
+static void UpdateDrawFrame(GameState* gs);
+
 
 int main() {
-	const int screenWidth = 800;
-	const int screenHeight = 450;
+    SetRandomSeed(0x69420);
+    const int screenWidth = 800;
+    const int screenHeight = 600;
 
-	InitWindow(screenWidth, screenHeight, "Splander");
+    InitWindow(screenWidth, screenHeight, "Splander");
+    SetTargetFPS(60);
 
-	camera.position = (Vector3){ 3.0f, 3.0f, 2.0f };
-	camera.target = (Vector3){ 0.0f, 0.0f, 0.0f };
-	camera.up = (Vector3){ 0.0f, 1.0f, 0.0f };
-	camera.fovy = 60.0f;
-	camera.projection = CAMERA_PERSPECTIVE;
-	while (!WindowShouldClose()) {
-		UpdateDrawFrame();
-	}
+    GameState* gs = malloc(sizeof(GameState));
+    gs->player.size.x = 50;
+    gs->player.size.y = 50;
+    gs->player.pos.x = GetScreenWidth() / 2.0f - (gs->player.size.x / 2);
+    gs->player.pos.y = GetScreenHeight() / 2.0f - (gs->player.size.y / 2);
 
-	CloseWindow();
+    while(!WindowShouldClose()) {
+        UpdateDrawFrame(gs);
+    }
 
-	return 0;
+    free(gs);
+    CloseWindow();
+
+    return 0;
 }
 
 // Update and draw game frame
-static void UpdateDrawFrame(void) {
-	UpdateCamera(&camera, CAMERA_ORBITAL);
-	BeginDrawing();
+static void UpdateDrawFrame(GameState* gs) {
+    
+    if (IsKeyDown(KEY_RIGHT)) gs->player.pos.x += 2.0f;
+    if (IsKeyDown(KEY_LEFT))  gs->player.pos.x -= 2.0f;
+    if (IsKeyDown(KEY_UP))    gs->player.pos.y -= 2.0f;
+    if (IsKeyDown(KEY_DOWN))  gs->player.pos.y += 2.0f;
 
-	ClearBackground(DARKGRAY);
+    BeginDrawing();
+        ClearBackground(GetColor(0x1E1E1EFF));
 
-	BeginMode3D(camera);
-
-	DrawCube(cubePosition, 2.0f, 2.0f, 2.0f, RED);
-	DrawCubeWires(cubePosition, 2.0f, 2.0f, 2.0f, MAROON);
-	DrawGrid(10, 1.0f);
-
-	EndMode3D();
-
-	DrawText("Welcome to raylib basic sample", 10, 40, 20, WHITE);
+        DrawRectangleV(gs->player.pos, gs->player.size, GetColor(0xAC29E2FF));
 
 	DrawFPS(10, 10);
 
