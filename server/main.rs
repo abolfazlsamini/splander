6ab75c0847b9fdcce2e519db6f62e6ce5b4e9225
mahanfr <mahan.farzaneh@gmail.com>
2024-12-16 13:13:33 +0330

    Rust Server Broadcast data to all clients with time

--- server/main.rs
+++ server/main.rs
@@ -1,16 +1,28 @@
 use tokio::net::UdpSocket;
-use std::io;
+use std::{collections::HashMap, io, time::{SystemTime, UNIX_EPOCH}};
 
 #[tokio::main]
 async fn main() -> io::Result<()> {
     let socket = UdpSocket::bind("0.0.0.0:8080").await?;
     let mut buf = [0; 1024];
+    let mut users_map = HashMap::<String, usize>::new();
+    let mut last_id = 0;
 
     loop {
         let (len, addr) = socket.recv_from(&mut buf).await?;
         println!("{:?} bytes recevied from {:?}",len , addr);
+        users_map.entry(addr.to_string()).or_insert(last_id);
+        last_id += 1;
 
-        let len = socket.send_to(&buf[..len], addr).await?;
-        println!("{:?} bytes sent",len);
+        for user in users_map.keys() {
+            let mut data = Vec::new();
+            let start = SystemTime::now();
+            let since_the_epoch = start.duration_since(UNIX_EPOCH).unwrap();
+            data.append(&mut since_the_epoch.as_millis().to_string().as_bytes().to_vec());
+            data.push(':' as u8);
+            data.append(&mut buf[..len].to_vec());
+            let len = socket.send_to(&data, user).await?;
+            println!("{:?} bytes sent to {}", len, user);
+        }
     }
 }
--- test/client.py
+++ test/client.py
@@ -1,15 +1,37 @@
 import socket
+import time
+import sys
 
-message = "Hello!!"
-bytes_to_send = str.encode(message)
+msgs1 = ["1", "2", "3", "4"]
+msgs2 = ["CAT", "DOG", "ZIBRA", "BIRD"]
+
+if len(sys.argv) > 1:
+    if sys.argv[1] == "1":
+        msgs = msgs1
+    else:
+        msgs = msgs2
+
 server_addr_port = ("0.0.0.0", 8080)
 buffer_size = 1024
 
 udp_client_scoket = socket.socket(
         family=socket.AF_INET, type=socket.SOCK_DGRAM)
 
+index = 0
+bytes_to_send = str.encode("INITIAL")
 udp_client_scoket.sendto(bytes_to_send, server_addr_port)
+while True:
+    if len(sys.argv) > 1:
+        msg = msgs[index]
+        index += 1
+        if index > 3:
+            index = 0
 
-msg_from_server = udp_client_scoket.recvfrom(buffer_size)
+        time.sleep(1)
 
-print(f"server: {msg_from_server[0]}")
+        bytes_to_send = str.encode(msg)
+        udp_client_scoket.sendto(bytes_to_send, server_addr_port)
+
+    msg_from_server = udp_client_scoket.recvfrom(buffer_size)
+
+    print(f"server: {msg_from_server[0]}")
