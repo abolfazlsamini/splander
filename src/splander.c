d4075d1082e90cbb902fa47905862c7cb2f52332
abolfazlssamini <abolfazl.samini78@gmail.com>
2025-01-24 11:50:59 +0330

    second player

--- src/splander.c
+++ src/splander.c
@@ -30,17 +30,17 @@
 
 typedef struct
 {
-    Vector2 pos;
-    Vector2 size;
+	Vector2 pos;
+	Vector2 size;
 } Player;
 
 typedef struct
 {
-    Player player;
-    Player player2;
-    Gui gui;
-    bool is_paused;
-    bool should_close;
+	Player player;
+	Player player2;
+	Gui gui;
+	bool is_paused;
+	bool should_close;
 } GameState;
 
 // Update and draw one frame
@@ -52,40 +52,43 @@ int client_socket;
 #endif
 struct sockaddr_in server;
 
-void init_socket() {
+void init_socket()
+{
 #if defined(_WIN32) || defined(_WIN64)
-    WSADATA wsa;
-    if (WSAStartup(MAKEWORD(2, 2), &wsa) != 0)
-    {
-        printf("Failed to initialize Winsock. Error: %d\n", WSAGetLastError());
-        return 1;
-    }
+	WSADATA wsa;
+	if (WSAStartup(MAKEWORD(2, 2), &wsa) != 0)
+	{
+		printf("Failed to initialize Winsock. Error: %d\n", WSAGetLastError());
+		exit(1);
+	}
 
-    // Create a socket
-    if ((client_socket = socket(AF_INET, SOCK_DGRAM, 0)) == INVALID_SOCKET)
-    {
-        printf("Failed to create socket. Error: %d\n", WSAGetLastError());
-        WSACleanup();
-        exit(1);
-    }
+	// Create a socket
+	if ((client_socket = socket(AF_INET, SOCK_DGRAM, 0)) == INVALID_SOCKET)
+	{
+		printf("Failed to create socket. Error: %d\n", WSAGetLastError());
+		WSACleanup();
+		exit(1);
+	}
 #else
-    // Create a socket
-    if ((client_socket = socket(AF_INET, SOCK_DGRAM, 0)) < 0)
-    {
-        printf("Failed to create socket. Error: %m\n");
-        exit(1);
-    }
+	// Create a socket
+	if ((client_socket = socket(AF_INET, SOCK_DGRAM, 0)) < 0)
+	{
+		printf("Failed to create socket. Error: %m\n");
+		exit(1);
+	}
 #endif
 }
 
 #if defined(_WIN32) || defined(_WIN64)
-void close_socket(SOCKET socket) {
-    closesocket(client_socket);
-    WSACleanup();
+void close_socket(SOCKET socket)
+{
+	closesocket(client_socket);
+	WSACleanup();
 }
 #else
-void close_socket(int c_socket) {
-    close(c_socket);
+void close_socket(int c_socket)
+{
+	close(c_socket);
 }
 #endif
 
@@ -93,68 +96,73 @@ char buffer[BUFFER_SIZE];
 char name[] = "Sam";
 int main()
 {
-    // Initialize sock
-    init_socket();
-    // Prepare the sockaddr_in structure
-    server.sin_family = AF_INET;
-    server.sin_addr.s_addr = inet_addr(SERVER_IP);
-    server.sin_port = htons(PORT);
+	// Initialize sock
+	init_socket();
+	// Prepare the sockaddr_in structure
+	server.sin_family = AF_INET;
+	server.sin_addr.s_addr = inet_addr(SERVER_IP);
+	server.sin_port = htons(PORT);
 
-    SetRandomSeed(0x69420);
-    const int screenWidth = 800;
-    const int screenHeight = 600;
+	SetRandomSeed(0x69420);
+	const int screenWidth = 800;
+	const int screenHeight = 600;
 
-    InitWindow(screenWidth, screenHeight, "Splander");
-    SetExitKey(KEY_F1);
-    SetTargetFPS(60);
+	InitWindow(screenWidth, screenHeight, "Splander");
+	SetExitKey(KEY_F1);
+	SetTargetFPS(60);
 
-    GameState gs = {0};
-    GUIInit(&gs.gui);
-    gs.player.size.x = 50;
-    gs.player.size.y = 50;
-    gs.player.pos.x = GetScreenWidth() / 2.0f - (gs.player.size.x / 2);
-    gs.player.pos.y = GetScreenHeight() / 2.0f - (gs.player.size.y / 2);
-    gs.is_paused = true;
+	GameState gs = {0};
+	GUIInit(&gs.gui);
+	gs.player.size.x = 50;
+	gs.player.size.y = 50;
+	gs.player.pos.x = GetScreenWidth() / 2.0f - (gs.player.size.x / 2) - 30.0f;
+	gs.player.pos.y = GetScreenHeight() / 2.0f - (gs.player.size.y / 2);
 
-    while (!WindowShouldClose() && !gs.should_close)
-    {
-        UpdateDrawFrame(&gs);
-    }
+	gs.player2.size.x = 50;
+	gs.player2.size.y = 50;
+	gs.player2.pos.x = GetScreenWidth() / 2.0f - (gs.player2.size.x / 2) + 30.0f;
+	gs.player2.pos.y = GetScreenHeight() / 2.0f - (gs.player2.size.y / 2);
+	gs.is_paused = true;
 
-    CloseWindow();
+	while (!WindowShouldClose() && !gs.should_close)
+	{
+		UpdateDrawFrame(&gs);
+	}
 
-    return 0;
+	CloseWindow();
+
+	return 0;
 }
 
 void GUIFunctionEval(GameState *gs)
 {
-    if (gs->gui.sel_page == 0)
-    {
-        switch (gs->gui.sel_button)
-        {
-            case 0:
-                gs->is_paused = !gs->is_paused;
-                char new_name[16];
-                strcpy(new_name, "name:");
-                strcat(new_name, name);
-                if (sendto(client_socket, new_name, 8, 0, (struct sockaddr *)&server, sizeof(server)) == SOCKET_ERROR)
-                {
-                    printf("sendto() failed. Error\n");
-                    close_socket(client_socket);
-                }
-                break;
-            case 1:
-            case 2:
-                printf("[Splander LOG] Button no Function!\n");
-                break;
-            case 3:
-                gs->should_close = true;
-                break;
-            default:
-                printf("[Splander ERROR] button has no function assigned! \n");
-                break;
-        }
-    }
+	if (gs->gui.sel_page == 0)
+	{
+		switch (gs->gui.sel_button)
+		{
+		case 0:
+			gs->is_paused = !gs->is_paused;
+			char new_name[16];
+			strcpy(new_name, "name:");
+			strcat(new_name, name);
+			if (sendto(client_socket, new_name, 8, 0, (struct sockaddr *)&server, sizeof(server)) == SOCKET_ERROR)
+			{
+				printf("sendto() failed. Error\n");
+				close_socket(client_socket);
+			}
+			break;
+		case 1:
+		case 2:
+			printf("[Splander LOG] Button no Function!\n");
+			break;
+		case 3:
+			gs->should_close = true;
+			break;
+		default:
+			printf("[Splander ERROR] button has no function assigned! \n");
+			break;
+		}
+	}
 }
 
 struct timeval milli_time;
@@ -162,128 +170,129 @@ double millisec = 0;
 time_t currentTime;
 static void UpdateServer(GameState *gs)
 {
-    time(&currentTime);
-    struct tm *localTime = localtime(&currentTime);
-    int hour = localTime->tm_hour;
-    int minute = localTime->tm_min;
-    int second = localTime->tm_sec;
+	time(&currentTime);
+	struct tm *localTime = localtime(&currentTime);
+	int hour = localTime->tm_hour;
+	int minute = localTime->tm_min;
+	int second = localTime->tm_sec;
 
 #if defined(_WIN32) || defined(_WIN64)
-    mingw_gettimeofday(&milli_time, NULL);
+	mingw_gettimeofday(&milli_time, NULL);
 #else
-    gettimeofday(&milli_time, NULL);
+	gettimeofday(&milli_time, NULL);
 #endif
-    millisec = (double)(milli_time.tv_usec);
-    // printf("Current time: %02d:%02d:%02d:%f\n", hour, minute, second, millisec);
+	millisec = (double)(milli_time.tv_usec);
+	// printf("Current time: %02d:%02d:%02d:%f\n", hour, minute, second, millisec);
 
-    char send_data[240];
-    char sent_time_str[100];
-    // TODO: change this to EPOCH TIME
-    sprintf(sent_time_str, "%02d:%02d:%02d:%.2f", hour, minute, second, millisec);
-    // name:time:position
-    sprintf(send_data, "%s,%s,%.2f:%.2f", name, sent_time_str, gs->player.pos.x, gs->player.pos.y);
-    printf("data sent: %s\n",send_data);
+	char send_data[240];
+	char sent_time_str[100];
+	// TODO: change this to EPOCH TIME
+	sprintf(sent_time_str, "%02d:%02d:%02d:%.2f", hour, minute, second, millisec);
+	// name:time:position
+	sprintf(send_data, "%s,%s,%.2f:%.2f", name, sent_time_str, gs->player.pos.x, gs->player.pos.y);
+	printf("data sent: %s\n", send_data);
 
-    // strcat(send_data, name);
-    // strcat(send_data, ",");
-    // strcat(send_data, sent_time_str);
-    // strcat(send_data, ",");
+	// strcat(send_data, name);
+	// strcat(send_data, ",");
+	// strcat(send_data, sent_time_str);
+	// strcat(send_data, ",");
 
-    // char player_pos_str[40];
-    // sprintf(player_pos_str, "%.2f:%.2f", gs->player.pos.x, gs->player.pos.y);
-    // strcat(send_data, player_pos_str);
-                                          // strcat(send_data, x);
-                                          // strcat(send_data, ":");
-                                          // strcat(send_data, y);
+	// char player_pos_str[40];
+	// sprintf(player_pos_str, "%.2f:%.2f", gs->player.pos.x, gs->player.pos.y);
+	// strcat(send_data, player_pos_str);
+	// strcat(send_data, x);
+	// strcat(send_data, ":");
+	// strcat(send_data, y);
 
-                                          // put all syncy data here  V
-    if (sendto(client_socket, send_data, sizeof(send_data) / sizeof(char), 0, (struct sockaddr *)&server, sizeof(server)) == SOCKET_ERROR)
-    {
-        printf("sendto() failed. Error\n");
-        close_socket(client_socket);
-    }
+	// put all syncy data here  V
+	if (sendto(client_socket, send_data, sizeof(send_data) / sizeof(char), 0, (struct sockaddr *)&server, sizeof(server)) == SOCKET_ERROR)
+	{
+		printf("sendto() failed. Error\n");
+		close_socket(client_socket);
+	}
 }
-static void UpdateClient()
-{
-    char receivdata[] = "name:time:position"; // only receive the other player's pos
-    char *name, *time, *position;
-    name = strtok(receivdata, ":");
-    time = strtok(receivdata, ":");
-    position = strtok(receivdata, ":");
+// static void UpdateClient()
+// {
+// 	char receivdata[] = "name:time:position"; // only receive the other player's pos
+// 	char *name, *time, *position;
+// 	name = strtok(receivdata, ":");
+// 	time = strtok(receivdata, ":");
+// 	position = strtok(receivdata, ":");
 
-    // if date > dateprev
+// 	// if date > dateprev
 
-    /*
-       int server_len = sizeof(server);
-       int recv_len;
-       if ((recv_len = recvfrom(client_socket, buffer, BUFFER_SIZE, 0, (struct sockaddr*)&server, &server_len)) == SOCKET_ERROR) {
-       printf("recvfrom() failed. Error: %d\n", WSAGetLastError());
-       } else {
-       buffer[recv_len] = '\0'; // Null-terminate the string
-       printf("Response from server: %s\n", buffer);
-       }
-       */
-}
+// 	/*
+// 	   int server_len = sizeof(server);
+// 	   int recv_len;
+// 	   if ((recv_len = recvfrom(client_socket, buffer, BUFFER_SIZE, 0, (struct sockaddr*)&server, &server_len)) == SOCKET_ERROR) {
+// 	   printf("recvfrom() failed. Error: %d\n", WSAGetLastError());
+// 	   } else {
+// 	   buffer[recv_len] = '\0'; // Null-terminate the string
+// 	   printf("Response from server: %s\n", buffer);
+// 	   }
+// 	   */
+// }
 // Update and draw game frame
 static void UpdateDrawFrame(GameState *gs)
 {
-    UpdateClient();
-    if (!gs->is_paused)
-    {
-        if (IsKeyDown(KEY_RIGHT))
-            gs->player.pos.x += 2.0f;
-        if (IsKeyDown(KEY_LEFT))
-            gs->player.pos.x -= 2.0f;
-        if (IsKeyDown(KEY_UP))
-            gs->player.pos.y -= 2.0f;
-        if (IsKeyDown(KEY_DOWN))
-            gs->player.pos.y += 2.0f;
+	// UpdateClient();
+	if (!gs->is_paused)
+	{
+		if (IsKeyDown(KEY_RIGHT))
+			gs->player.pos.x += 2.0f;
+		if (IsKeyDown(KEY_LEFT))
+			gs->player.pos.x -= 2.0f;
+		if (IsKeyDown(KEY_UP))
+			gs->player.pos.y -= 2.0f;
+		if (IsKeyDown(KEY_DOWN))
+			gs->player.pos.y += 2.0f;
 
-        if (IsKeyDown(KEY_RIGHT) ||
-                IsKeyDown(KEY_LEFT) ||
-                IsKeyDown(KEY_UP) ||
-                IsKeyDown(KEY_DOWN))
-            UpdateServer(gs);
-    }
+		if (IsKeyDown(KEY_RIGHT) ||
+			IsKeyDown(KEY_LEFT) ||
+			IsKeyDown(KEY_UP) ||
+			IsKeyDown(KEY_DOWN))
+			UpdateServer(gs);
+	}
 
-    else
-    {
-        if (IsKeyPressed(KEY_UP))
-        {
-            gs->gui.sel_button -= 1;
-            if (gs->gui.sel_button < 0)
-            {
-                gs->gui.sel_button = gs->gui.items[gs->gui.sel_page].count - 1;
-            }
-        }
-        if (IsKeyPressed(KEY_DOWN))
-        {
-            gs->gui.sel_button += 1;
-            if (gs->gui.sel_button >= gs->gui.items[gs->gui.sel_page].count)
-            {
-                gs->gui.sel_button = 0;
-            }
-        }
-        if (IsKeyPressed(KEY_ENTER))
-        {
-            GUIFunctionEval(gs);
-        }
-    }
+	else
+	{
+		if (IsKeyPressed(KEY_UP))
+		{
+			gs->gui.sel_button -= 1;
+			if (gs->gui.sel_button < 0)
+			{
+				gs->gui.sel_button = gs->gui.items[gs->gui.sel_page].count - 1;
+			}
+		}
+		if (IsKeyPressed(KEY_DOWN))
+		{
+			gs->gui.sel_button += 1;
+			if (gs->gui.sel_button >= gs->gui.items[gs->gui.sel_page].count)
+			{
+				gs->gui.sel_button = 0;
+			}
+		}
+		if (IsKeyPressed(KEY_ENTER))
+		{
+			GUIFunctionEval(gs);
+		}
+	}
 
-    if (IsKeyPressed(KEY_ESCAPE))
-        gs->is_paused = !gs->is_paused;
+	if (IsKeyPressed(KEY_ESCAPE))
+		gs->is_paused = !gs->is_paused;
 
-    BeginDrawing();
-    ClearBackground(GetColor(0x1E1E1EFF));
+	BeginDrawing();
+	ClearBackground(GetColor(0x1E1E1EFF));
 
-    DrawRectangleV(gs->player.pos, gs->player.size, GetColor(0xffffff));
+	DrawRectangleV(gs->player.pos, gs->player.size, GetColor(0xffffff));
+	DrawRectangleV(gs->player2.pos, gs->player2.size, GetColor(0xffaaff));
 
-    DrawFPS(10, 10);
+	DrawFPS(10, 10);
 
-    if (gs->is_paused)
-    {
-        GUIDraw(gs->gui);
-    }
+	if (gs->is_paused)
+	{
+		GUIDraw(gs->gui);
+	}
 
-    EndDrawing();
+	EndDrawing();
 }
